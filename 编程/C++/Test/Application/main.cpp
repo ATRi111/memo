aa1f15eb1a77e81e8d1853b07db4407b2d4146df
ATRi111 <1657125180@qq.com>
2024-06-17 16:33:46 +0800

    新增宏，模板，多返回值

--- 编程/C++/Test/Application/main.cpp
+++ 编程/C++/Test/Application/main.cpp
@@ -4,7 +4,16 @@
 #include"glfw3.h"
 using namespace std;
 
+#define DEBUGMODE 0
+
+#if DEBUGMODE == 1
+    #define LOG(x) cout << x << endl;
+#else
+    #define LOG(x)
+#endif
+
 int main()
 {
-	int a = glfwInit();
+ 	int a = 1;
+    LOG(a);
 }
