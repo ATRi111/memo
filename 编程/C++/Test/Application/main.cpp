b97dd05a8fe9290b64611b9d92799d72e3d071da
1657125180@qq.com <1657125180@qq.com>
2024-06-23 20:51:06 +0800

    ——

--- 编程/C++/Test/Application/main.cpp
+++ 编程/C++/Test/Application/main.cpp
@@ -1,48 +1,15 @@
 ﻿#include<iostream>
 #include<vector>
-#include<algorithm>
-#include<functional>
+#include<string>
 #include "Library.h"
 using namespace std;
 
-class Test
-{
-public:
-	int* a;
-	Test()
-	{
-		a = new int(0);
-		cout << "默认" << endl;
-	}
-	Test(const Test& t)
-	{
-		a = new int(*(t.a));
-		cout << "拷贝" << endl;
-	}
-	Test(Test&& t) noexcept
-	{
-		a = t.a;
-		t.a = nullptr;
-		cout << "移动" << endl;
-	}
-	~Test()
-	{
-		delete a;
-		cout << "析构" << endl;
-	}
-};
-
-class C
-{
-public:
-	Test t;
-	C()
-	{
-		
-	}
-};
 
 int main()
 {
-	C c;
+	string s1 = "Hello";
+	string s2 = "World";
+	cout << &s2 << endl;
+	s2 += s1;
+	cout << &s2 << endl;
 }
